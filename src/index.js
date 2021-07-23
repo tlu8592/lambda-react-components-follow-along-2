f6f52a0372fbfd64c3974a0fea6347a6365fd512
Tony Lu <70731161+tlu8592@users.noreply.github.com>
2021-07-23 06:17:10 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles.css";
+
+const white = "https://image.flaticon.com/icons/png/512/32/32177.png";
+const yellow =
+  "https://i.pinimg.com/originals/92/94/ba/9294badee7b8f3d93fa9bc6c874641b2.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={white} />
+      <img src={yellow} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
